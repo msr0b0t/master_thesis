597aacaa0ccae37190ddfaf8d93c0868131b293b
Mary Kouvela <marykouvela@gmail.com>
2020-01-24 20:56:18 +0200

    updates

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from get_predictions import predict as pr
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+# from pymongo import MongoClient
+# from bson import ObjectId
 
 app = Flask(__name__)
 cors = CORS(app)
+# client = MongoClient()
+# db = client["feedback-db"]
 
 @app.route('/', methods=['GET'])
 def hello_world():
@@ -13,6 +17,17 @@ def hello_world():
 @app.route('/predict/<username>/', methods=['GET'])
 def predict(username):
     return jsonify(pr(username))
+
+
+# @app.route('/feedback', methods=['POST'])
+# def feedback():
+#     data = request.get_json(force=True)
+#     db.feedback.insert_one({
+#         "user_selection": data["user_selection"],
+#         "analysis_id": ObjectId(data["analysis_id"]),
+#         "reasons": {reason.replace(".", ""): value for [reason, value] in data["reasons"]}
+#     })
+#     return jsonify({"ok": True})
 
 
 if __name__ == '__main__':
